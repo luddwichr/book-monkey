d7231f691fbc021c185a6aa31e565bf24653d2d2
luddwichr <luddwichr@users.noreply.github.com>
2019-03-06 23:10:32 +0100

    implemented book list acording to chapter 6.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {BookListComponent} from './book-list/book-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, BookListComponent
       ],
     }).compileComponents();
   }));
@@ -14,18 +15,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'BookMonkey'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('BookMonkey');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to BookMonkey!');
   });
 });
---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../shared/book';
+import {Thumbnail} from '../shared/thumbnail';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: './book-list.component.html',
+  styles: []
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.books = [
+      new Book('1234567890123',
+        'Angular',
+        ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'],
+        new Date(2017, 3, 1), 'Grundlagen, blabla',
+        5,
+        [new Thumbnail('https://ng-buch.de/cover2.jpg', 'Buchcover')],
+        'Lorem ipsum dolor sit amet, consectetur adipisici elit,...'),
+      new Book('0987654321',
+        'AngularJS',
+        ['Philipp Tarasiewicz', 'Robin Böhm'],
+        new Date(2014, 5, 29),
+        'Eine praktische Einführung',
+        4.5,
+        [new Thumbnail('https://ng-buch.de/cover1.jpg', 'Buchcover')],
+        'Dieses Buch führt Sie anhand eines zusammenhängennden Beispielprojekts...'
+      )
+    ];
+  }
+
+}
