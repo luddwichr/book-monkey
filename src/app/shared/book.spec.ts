896288d81744c087ae275a148c1dd00012ac61fd
luddwichr <luddwichr@users.noreply.github.com>
2019-03-07 13:02:56 +0100

    changed book and thumbnail to interfaces

--- src/app/shared/book.spec.ts
+++
@@ -1,7 +0,0 @@
-import {Book} from './book';
-
-describe('Book', () => {
-  it('should create an instance', () => {
-    expect(new Book('123456789123', 'someTitle', [], new Date())).toBeTruthy();
-  });
-});
--- src/app/shared/thumbnail.spec.ts
+++
@@ -1,9 +0,0 @@
-import {Thumbnail} from './thumbnail';
-
-export {Thumbnail} from './thumbnail';
-
-describe('Thumbnail', () => {
-  it('should create an instance', () => {
-    expect(new Thumbnail('x.y.com', 'someTitle')).toBeTruthy();
-  });
-});
