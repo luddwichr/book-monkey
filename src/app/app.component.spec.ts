8805702dbae03c5f0ec4a0bbb75f9fd2a8bb9573
luddwichr <luddwichr@users.noreply.github.com>
2019-03-07 15:01:14 +0100

    implemented stuff according to chapter 6.3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import {AppComponent} from './app.component';
 import {BookListComponent} from './book-list/book-list.component';
 import {MockComponent, MockedComponent} from 'ng-mocks';
 import {By} from '@angular/platform-browser';
+import {BookDetailsComponent} from './book-details/book-details.component';
+import {Book} from './shared/book';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -10,7 +12,7 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent, MockComponent(BookListComponent)]
+      declarations: [AppComponent, MockComponent(BookListComponent), MockComponent(BookDetailsComponent)]
     }).compileComponents();
   }));
 
@@ -24,8 +26,44 @@ describe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should contain a book list', () => {
-    const bookList = fixture.debugElement.query(By.css('bm-book-list')).componentInstance as BookListComponent;
-    expect(bookList).toBeTruthy();
+  it('should initially render only the book list', () => {
+    expect(getBookList()).toBeTruthy();
+    expect(getBookDetais()).toBeFalsy();
   });
+
+  it('should render book details when a book list entry triggered a show book details event', () => {
+    const book: Book = {isbn: '', title: '', authors: [], published: new Date()};
+
+    getBookList().showDetailsEvent.emit(book);
+    fixture.detectChanges();
+
+    const bookDetailsComponent = getBookDetais();
+    expect(bookDetailsComponent).toBeTruthy();
+    expect(bookDetailsComponent.book).toBe(book);
+    expect(getBookList()).toBeFalsy();
+  });
+
+  it('should render a book list when triggered by book details', () => {
+    getBookList().showDetailsEvent.emit();
+    fixture.detectChanges();
+
+    getBookDetais().showListEvent.emit();
+    fixture.detectChanges();
+
+    expect(getBookList()).toBeTruthy();
+    expect(getBookDetais()).toBeFalsy();
+  });
+
+  function getBookList(): BookListComponent {
+    return getComponent<BookListComponent>('bm-book-list');
+  }
+
+  function getBookDetais(): BookDetailsComponent {
+    return getComponent<BookDetailsComponent>('bm-book-details');
+  }
+
+  function getComponent<T>(selector: string): T {
+    const element = fixture.debugElement.query(By.css(selector));
+    return element ? (element.componentInstance as T) : null;
+  }
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import {Component} from '@angular/core';
+import {Book} from './shared/book';
 
 @Component({
   selector: 'bm-root',
-  template: '<bm-book-list></bm-book-list>',
+  template: `
+    <bm-book-list *ngIf="listOn" (showDetailsEvent)="showDetails($event)"> </bm-book-list>
+    <bm-book-details *ngIf="detailsOn" [book]="book" (showListEvent)="showList()"> </bm-book-details>
+  `,
   styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  listOn = true;
+  detailsOn = false;
+  book: Book;
+
+  showList() {
+    this.listOn = true;
+    this.detailsOn = false;
+  }
+
+  showDetails(book: Book) {
+    this.book = book;
+    this.listOn = false;
+    this.detailsOn = true;
+  }
+}
