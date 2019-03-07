0035a3e7d63fdbe8dd0195ceb5cb01b1dc440af5
Ludwig Richter <ludwig.richter@andrena.de>
2019-03-07 13:03:38 +0100

    introduced ng-mocks and fixed/improved tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,31 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, TestBed, ComponentFixture} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {BookListComponent} from './book-list/book-list.component';
+import {MockComponent, MockedComponent} from 'ng-mocks';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent, BookListComponent]
+      declarations: [AppComponent, MockComponent(BookListComponent)]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain a book list', () => {
+    const bookList = fixture.debugElement.query(By.css('bm-book-list')).componentInstance as BookListComponent;
+    expect(bookList).toBeTruthy();
   });
 });
--- src/app/book-list-item/book-list-item.component.spec.ts
+++ src/app/book-list-item/book-list-item.component.spec.ts
@@ -1,24 +1,73 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {BookListItemComponent} from './book-list-item.component';
+import {MockRender} from 'ng-mocks';
+import {By} from '@angular/platform-browser';
+import {Book} from '../shared/book';
 
 describe('BookListItemComponent', () => {
-  let component: BookListItemComponent;
-  let fixture: ComponentFixture<BookListItemComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BookListItemComponent]
     }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should render a complete book', () => {
+    const book = {
+      ...basicBook(),
+      rating: 2.5,
+      thumbnails: [{url: 'someUrl', title: 'someImageTitle'}],
+      subtitle: 'someSubTitle',
+      description: 'someDescription'
+    };
+    const fixture = MockRender('<bm-book-list-item [book]=book></bm-book-list-item>', {book});
+    const componentInstance = fixture.debugElement.query(By.directive(BookListItemComponent))
+      .componentInstance as BookListItemComponent;
+    expect(componentInstance).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('div.header')).nativeElement.textContent).toEqual('someTitle');
+    expect(fixture.debugElement.query(By.css('div.description')).nativeElement.textContent).toEqual('someSubTitle');
+    expect(fixture.debugElement.query(By.css('div.metadata')).nativeElement.textContent).toEqual('A, B');
+    expect(fixture.debugElement.query(By.css('div.extra')).nativeElement.textContent).toEqual('ISBN 123');
+    expect(fixture.debugElement.query(By.css('img')).nativeElement.src).toContain('someUrl');
+    expect(fixture.debugElement.query(By.css('img')).nativeElement.alt).toEqual('someImageTitle');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not render a description section if no subtitle is provided for a book', () => {
+    const book = basicBook();
+    const fixture = MockRender('<bm-book-list-item [book]=book></bm-book-list-item>', {book});
+    const componentInstance = fixture.debugElement.query(By.directive(BookListItemComponent))
+      .componentInstance as BookListItemComponent;
+    expect(componentInstance).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('div.description'))).toBeNull();
   });
+
+  it('should not render a thumbnail image if no thumbnails are provided for a book', () => {
+    const book = basicBook();
+    const fixture = MockRender('<bm-book-list-item [book]=book></bm-book-list-item>', {book});
+    const componentInstance = fixture.debugElement.query(By.directive(BookListItemComponent))
+      .componentInstance as BookListItemComponent;
+    expect(componentInstance).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('img'))).toBeNull();
+  });
+
+  it('should not render a thumbnail image if an empty thumbnail array is provided for a book', () => {
+    const book = {
+      ...basicBook(),
+      thumbnails: []
+    };
+    const fixture = MockRender('<bm-book-list-item [book]=book></bm-book-list-item>', {book});
+    const componentInstance = fixture.debugElement.query(By.directive(BookListItemComponent))
+      .componentInstance as BookListItemComponent;
+    expect(componentInstance).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('img'))).toBeNull();
+  });
+
+  function basicBook(): Book {
+    return {
+      isbn: '123',
+      title: 'someTitle',
+      authors: ['A', 'B'],
+      published: new Date(2019, 4, 30)
+    };
+  }
 });
