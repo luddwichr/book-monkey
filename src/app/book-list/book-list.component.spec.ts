dfe5d28163d440a26357ac1d8742764c5f93d048
luddwichr <luddwichr@users.noreply.github.com>
2019-03-07 13:03:38 +0100

    introduced ng-mocks and fixed/improved tests

--- src/app/book-list/book-list.component.spec.ts
+++ src/app/book-list/book-list.component.spec.ts
@@ -1,5 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {BookListComponent} from './book-list.component';
+import {BookListItemComponent} from '../book-list-item/book-list-item.component';
+import {MockComponent, MockedComponent} from 'ng-mocks';
+import {By} from '@angular/platform-browser';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
@@ -7,7 +10,7 @@ describe('BookListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookListComponent]
+      declarations: [BookListComponent, MockComponent(BookListItemComponent)]
     }).compileComponents();
   }));
 
@@ -19,5 +22,14 @@ describe('BookListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain two book list items, each for one of the books', () => {
+    const bookListItems = fixture.debugElement
+      .queryAll(By.css('bm-book-list-item'))
+      .map(element => element.componentInstance as BookListItemComponent);
+    expect(bookListItems).toHaveLength(2);
+    expect(bookListItems[0].book).toBe(component.books[0]);
+    expect(bookListItems[1].book).toBe(component.books[1]);
   });
 });
